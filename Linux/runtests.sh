4eab38defdade36ee16b1aefe355cebfca04c66a
Pavol Juhas <pavol.juhas@gmail.com>
2013-10-28 23:31:46 -0400

    Do not test PDFgui if not included.
    Make sure diffpy.Structure gets tested.

--- Linux/runtests.sh
+++ Linux/runtests.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-python -m diffpy.pdfgui.tests.rundeps
+# python -m diffpy.pdfgui.tests.rundeps
+python -m diffpy.Structure.tests.run
 python -m diffpy.srreal.tests.run
 python -m diffpy.srfit.tests.run
-python -m diffpy.pdfgetx.tests.run
+
+# FIXME ... add pyobjcryst unit tests.
