fe6b2f018a0576172823de85aef0fff6d5ccdf7a
Xiaohao Yang <sodestiny1@gmail.com>
2013-11-02 19:49:05 -0400

    create directory before copy file

--- Linux/03-package.zsh
+++ Linux/03-package.zsh
@@ -6,6 +6,7 @@ mv bin $srfitpath/bin
 mv include $srfitpath/include
 mv lib $srfitpath/lib
 mv share $srfitpath/share
+mkdir -p $srfitpath/src/cctbx
 mv src/cctbx/cctbx_build $srfitpath/src/cctbx/cctbx_build
 
 cp /usr/lib/libboost_python-py27.so.1.46.1 $srfitpath/lib
@@ -14,10 +15,9 @@ cp diffpy-u12x64.pth $srfitpath/diffpy-u12x64.pth
 cp INSTALL.txt $srfitpath/INSTALL.txt
 cp runtests.sh $srfitpath/runtests.sh
 
-mkdir -p $srfitpath/src/cctbx
 cp src/cctbx/cctbx_bundle.tar.gz $srfitpath/src/cctbx
 ./01-fetchsources.zsh $srfitpath/src
 
 rm -rf $srfitpath/src/cctbx/cctbx_bundle.tar.gz
-
 tar jcvf $srfitpath.tar.bz2 $srfitpath
+rm -rf $srfitpath
