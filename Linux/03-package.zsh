b8d50da6d5589ebfb8fea5771f92133733c37051
Pavol Juhas <pavol.juhas@gmail.com>
2013-11-04 15:55:06 -0500

    Use new hard-linked tree instead of moving the files.

--- Linux/03-package.zsh
+++ Linux/03-package.zsh
@@ -1,23 +1,64 @@
 #!/bin/zsh -f
 
-srfitpath=srfit-$(date +%F)-$(uname -s)-$(uname -i)
-mkdir $srfitpath
-mv bin $srfitpath/bin
-mv include $srfitpath/include
-mv lib $srfitpath/lib
-mv share $srfitpath/share
-mkdir -p $srfitpath/src/cctbx
-mv src/cctbx/cctbx_build $srfitpath/src/cctbx/cctbx_build
+setopt err_exit
+setopt extendedglob
 
-cp /usr/lib/libboost_python-py27.so.1.46.1 $srfitpath/lib
-cp /usr/lib/libboost_serialization.so.1.46.1 $srfitpath/lib
-cp diffpy-u12x64.pth $srfitpath/diffpy-u12x64.pth
-cp INSTALL.txt $srfitpath/INSTALL.txt
-cp runtests.sh $srfitpath/runtests.sh
+MYDIR="$(cd ${0:h} && pwd)"
+cd $MYDIR
 
-cp src/cctbx/cctbx_bundle.tar.gz $srfitpath/src/cctbx
-./01-fetchsources.zsh $srfitpath/src
+PACKAGE=diffpy-1.0-$CPUTYPE-$(date '+%Y%m%d')
+mkdir -p $PACKAGE
+cp -rlu bin include lib share src *.pth runtests.sh INSTALL.txt $PACKAGE/
 
-rm -rf $srfitpath/src/cctbx/cctbx_bundle.tar.gz
-tar jcvf $srfitpath.tar.bz2 $srfitpath
-rm -rf $srfitpath
+# clean up intermediate object files in cctbx_build
+CCTBXBUILD=$PACKAGE/src/cctbx/cctbx_build
+CCTBXSOURCES=$PACKAGE/src/cctbx/cctbx_sources
+${CCTBXBUILD}/bin/libtbx.scons -c -C $CCTBXBUILD
+rm -vrf ${CCTBXSOURCES}/boost
+rm -vrf ${CCTBXSOURCES}/phenix_examples
+
+# clean up all git repositories
+for gd in ${PACKAGE}/src/*/.git(:h); do
+    (cd $gd && git clean -fdx)
+done
+
+# clean up pycifrw
+rm -vrf ${PACKAGE}/src/pycifrw/pycifrw/build
+
+# clean up build directories in all sans packages
+rm -vrf ${PACKAGE}/src/sans/data_util/build
+rm -vrf ${PACKAGE}/src/sans/pr_inversion/build
+rm -vrf ${PACKAGE}/src/sans/sansdataloader/build
+rm -vrf ${PACKAGE}/src/sans/sansmodels/build
+
+# clean up tar bundles from 01-fetchsources.zsh
+rm -vf $PACKAGE/src/cctbx/cctbx_bundle.tar.gz
+rm -vf $PACKAGE/src/cctbx/cctbx_install_script.csh
+rm -vf $PACKAGE/src/pyobjcryst/libobjcryst/newmat/newmat11.tar.gz
+
+# remove any pyc files
+find ${PACKAGE} -type f -name '*.pyc' -print0 | xargs -0 rm -v
+
+# check if any of the unwanted files are still around
+allfiles=( ${PACKAGE}/**/*~*.git*(.N) )
+undesired=(
+    ${PACKAGE}/**/build(N/)
+    ${PACKAGE}/**/*(-@N)
+    ${(M)allfiles:#*.o}
+    ${(M)allfiles:#*.py[oc]}
+    ${(M)allfiles:#*/cctbx_bundle.tar.gz}
+    ${(M)allfiles:#*/cctbx_install_script.csh}
+    ${(M)allfiles:#*/newmat11.tar.gz}
+)
+if [[ -n ${undesired} ]]; then
+    print -u2 "Some files were not cleaned up:"
+    print -u2 -l ${undesired}
+    exit 1
+fi
+
+# finally create the tar bundle
+tar czf ${PACKAGE}.tar.gz \
+    --numeric-owner --owner=0 --group=0 $PACKAGE
+
+# and remove the package directory itself
+rm -rf $PACKAGE
