d747e6b22d06b61413ec368b4e2f90041811c1ec
XiaohaoYang <sodestiny1@gmail.com>
2014-02-07 19:09:27 -0500

    update package script

--- Linux/03-package.zsh
+++ Linux/03-package.zsh
@@ -9,14 +9,14 @@ cd $MYDIR
 PACKAGE=diffpy-1.0-$CPUTYPE-$(date '+%Y%m%d')
 mkdir -p $PACKAGE
 cp -rlu bin include lib share src *.pth runtests.sh INSTALL.txt $PACKAGE/
-cp -rlu buildtools BUILD.txt update-srfit.zsh $PACKAGE/
+cp -rlu buildtools BUILD.txt update.zsh $PACKAGE/
 cp -rlu 00-clean.zsh 01-fetchsources.zsh 02-buildall.zsh $PACKAGE/buildtools
 
 # clean files
 ./00-clean.zsh $PACKAGE
 
-cp /usr/lib/libboost_python-py27.so.1.46.1 $PACKAGE/lib
-cp /usr/lib/libboost_serialization.so.1.46.1 $PACKAGE/lib
+cp /usr/lib/libboost_python-py27.so* $PACKAGE/lib
+cp /usr/lib/libboost_serialization.so* $PACKAGE/lib
 
 # finally create the tar bundle
 tar czf ${PACKAGE}.tar.gz \
