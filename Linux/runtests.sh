052ea0fd9d19058e83cb100b8fd39b3bcf623849
Pavol Juhas <pavol.juhas@gmail.com>
2014-03-04 15:44:09 -0500

    Execute pyobjcryst tests.

--- Linux/runtests.sh
+++ Linux/runtests.sh
@@ -1,8 +1,7 @@
 #!/bin/sh -x
 
-# python -m diffpy.pdfgui.tests.rundeps
 python -m diffpy.Structure.tests.run
+python -m diffpy.utils.tests.run
 python -m diffpy.srreal.tests.run
+python -m pyobjcryst.tests.run
 python -m diffpy.srfit.tests.run
-
-# FIXME ... add pyobjcryst unit tests.
