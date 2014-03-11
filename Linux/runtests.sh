50a49296d6ad02bbf1bc553cc3d668e99d60e35a
Pavol Juhas <pavol.juhas@gmail.com>
2014-03-11 11:35:57 -0400

    Print out what is being tested.

--- Linux/runtests.sh
+++ Linux/runtests.sh
@@ -1,7 +1,20 @@
-#!/bin/sh -x
+#!/bin/sh
 
+echo "# Testing diffpy.Structure:"
 python -m diffpy.Structure.tests.run
+echo
+
+echo "# Testing diffpy.utils:"
 python -m diffpy.utils.tests.run
+echo
+
+echo "# Testing diffpy.srreal:"
 python -m diffpy.srreal.tests.run
+echo
+
+echo "# Testing pyobjcryst:"
 python -m pyobjcryst.tests.run
+echo
+
+echo "# Testing diffpy.srfit:"
 python -m diffpy.srfit.tests.run
